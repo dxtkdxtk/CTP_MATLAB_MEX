9da80afaef71a29e3e2b49486f521b1c0747de4f
jebin <cepinlin@gmail.com>
2014-07-14 16:12:38 +0800

    add add Getorder

---
+++ GetAllOrders.m
@@ -0,0 +1,6 @@
+function orders = GetOrders()
+%GETORDERS 获取当前未结束所有合约
+orders = TraderMain(7);
+
+end
+
---
+++ GetOrder.m
@@ -0,0 +1,7 @@
+function order = GetOrder(orderref)
+%GETORDER 此处显示有关此函数的摘要
+%   此处显示详细说明
+
+order = TraderMain(12, orderref);
+end
+
